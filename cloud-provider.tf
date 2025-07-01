2fa8aa72ee00d281e3ef799e5707ae0c4635f5e8
sbglfj <jpx2b1@gmail.com>
2025-07-01 09:38:23 -0400

    Update cloud-provider.tf

--- cloud-provider.tf
+++ cloud-provider.tf
@@ -9,7 +9,7 @@ terraform {
 provider "grafana" {
   alias = "cloud"
 
-  cloud_access_policy_token = "glc_eyJvIjoiMTIyNTM3MSIsIm4iOiJmcm9udGVuZC1vYnNlcnZhYmlsaXR5LWdyYWZhbmEtY2xvdWQtYWNjZXNzLXRva2VuLWZyb250ZW5kLW9ic2VydmFiaWxpdHktZ3JhZmFuYS1jbG91ZC1hY2Nlc3MtcG9saWN5LXRva2VuIiwiayI6Ijc1ODlDMUtNOHZMVFZOeVEzM084NkpSVyIsIm0iOnsiciI6InByb2QtdXMtZWFzdC0wIn19"
+  cloud_access_policy_token = "lsa_mNP6lEHszsvtIVFJzSAqlxXhQWzFh3ig_715c2eaa"
 }
 
 data "grafana_cloud_stack" "stack" {
